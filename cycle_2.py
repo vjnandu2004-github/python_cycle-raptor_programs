4fffe4cb8afb45e8a4cf4725cf8193248a46160f
vjnandu2004-github <146189444+vjnandu2004-github@users.noreply.github.com>
2024-01-14 17:08:29 +0530

    Update cycle_2.py

--- cycle_2.py
+++ cycle_2.py
@@ -1,9 +1,21 @@
 import math
-a=int(input("enter the saide a"))
-b=int(input("enter the saide b"))
-c=int(input("enter the saide c"))
-s=(a+b+c)/2
-area=math.sqrt(s*(s-a)*(s-b)*(s-c))
-perimeter=a+b+c
-print(area)
-print(perimeter)
+def areaof_triangle(a,b,c):
+s=(a+b+c)//2
+Area=math.floor(math.sqrt(s*(s-a)*(s-b)*(s-c)))
+return Area
+mylst=[]
+for i in range(1,3):
+print("Trinangle-",i)
+a=int(input("Enter the side a:"))
+b=int(input("Enter the side b:"))
+c=int(input("Enter the side c:"))
+Area=areaof_triangle(a,b,c)
+mylst.append(Area)
+print("Area of Triangle 1-",mylst[0])
+print("Area of Triangle 2-",mylst[1])
+sum_of_area=sum(mylst)
+print("Sum of areas of 2 triangles: ",sum_of_area)
+ct1=round(((int(mylst[0]))/(sum_of_area))*100,2)
+ct2=round(((int(mylst[1]))/(sum_of_area))*100,2)
+print("Contribution of triangle 1=",ct1)
+print("Contribution of triangle 1=",ct2)
